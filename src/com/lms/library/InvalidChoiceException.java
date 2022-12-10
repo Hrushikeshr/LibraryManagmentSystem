68f4cf2257e68091ee5b468706c43eae4ffe402a
Hrushikeshr <hrushikesh.r@techouts.com>
2022-12-10 15:30:36 +0530

    Lms Upto Update method

---
+++ src/com/lms/library/InvalidChoiceException.java
@@ -0,0 +1,11 @@
+package com.lms.library;
+
+public class InvalidChoiceException extends RuntimeException {
+
+	@Override
+	public String toString() {
+		
+		return "";
+	}
+	
+}
---
+++ src/com/lms/main/MainClass.java
@@ -0,0 +1,15 @@
+package com.lms.main;
+
+import com.lms.library.Librarian;
+
+import java.util.ArrayList;
+
+public class MainClass {
+	public static void main(String[] agrs)
+	{
+		Librarian lib = new Librarian();
+		lib.start();
+	}
+}
+
+
